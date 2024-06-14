18e83b76765ac2559ff7d3df2b39be8b37041c79
bradybaughrobert-fs <rlbradybaugh@student.fullsail.edu>
2024-06-14 14:09:27 -0400

    Added new methods

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,38 +1,63 @@
 const express = require("express");
+const {v4: uuidv4} = require("uuid");   // import uuidv4 from 'uuid';
 const router = express.Router();
+
+const database = []; // this is our "database"
 
 // localhost:3000/api/
 router.get("/", (req, res) => {
   res.status(200).json({
-    message: "GET TO API",
+    message: "GET - API",
+    data: database, 
     metadata: { hostname: req.hostname, method: req.method },
   });
 });
 
 router.get("/:id", (req, res)=>{
-  const {id} = req.params.id;
-  res.status(200).json({
-    message: "GET - SUCCESS",
-    id: id,
-    metadata: {
-       hostname: req.hostname,
-       method: req.method,
-    },
-  });
+  const {id} = req.params;
+  const singleId = database.find((singleId)=> singleId.id === id); // find the id in the database
+  if (singleId){
+    res.status(200).json({
+      message: "GET - API by ID",
+      data: singleId,
+      metadata: {
+        hostname: req.hostname,
+        method: req.method,
+      },
+    });
+  };
+  
 });
 
 router.post("/", (req, res) => {
  const { data }= req.body;
- res.status(200).json({
-    message:"POST to /api", 
-    data,
+ database.push({id: uuidv4(), ...data}); // add the data to the database
+ res.status(201).json({
+    message:"POST - /api", 
+    data: database[database.length - 1], // return the last item in the database
     metadata: {hostname: req.hostname, method: req.method},
 });
-  res.status(200).json({
-    message: "POST TO API",
-    data: req.body
-  });
+  
 });
+
+router.patch("/:id", (req, res)=>{ // update select data in the database
+  const {id} = req.params;
+  const {data} = req.body;
+  const singleId = database.find((singleId)=> singleId.id === id); // find the id in the database
+  if (singleId){
+    database.push({id, ...data}); // add the data to the database
+    res.status(200).json({
+      message: "PATCH - API by ID",
+      data: singleId,
+      metadata: {
+        hostname: req.hostname,
+        method: req.method,
+      },
+    });
+  };
+  
+});
+
 
 
 module.exports = router;
