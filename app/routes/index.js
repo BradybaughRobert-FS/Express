841acc5307166ad439b383efc41118543533c772
bradybaughrobert-fs <rlbradybaugh@student.fullsail.edu>
2024-06-15 11:15:03 -0400

    made some changes to the code. All are working as inteded.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,63 +1,93 @@
-const express = require("express");
-const {v4: uuidv4} = require("uuid");   // import uuidv4 from 'uuid';
+const { v4: uuidv4 } = require('uuid');
+const express = require('express');
 const router = express.Router();
 
-const database = []; // this is our "database"
+let database = [];
 
-// localhost:3000/api/
-router.get("/", (req, res) => {
+// Assume context of 'users'
+
+router.get('/users', (req, res, next) => {
   res.status(200).json({
-    message: "GET - API",
-    data: database, 
-    metadata: { hostname: req.hostname, method: req.method },
+    message: `GET - API`,
+    database,
+    metadata: {
+      hostname: req.hostname,
+      method: req.method,
+    },
   });
 });
 
-router.get("/:id", (req, res)=>{
-  const {id} = req.params;
-  const singleId = database.find((singleId)=> singleId.id === id); // find the id in the database
-  if (singleId){
+router.get('/users/:id', (req, res, next) => {
+  const { id } = req.params;
+  const user = database.find((item) => item.id === id);
+  if (user) {
     res.status(200).json({
-      message: "GET - API by ID",
-      data: singleId,
+      message: `GET by ID - /api`,
+      data: user,
       metadata: {
         hostname: req.hostname,
         method: req.method,
       },
     });
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
+});
+
+router.post('/users', (req, res, next) => {
+  // user has name
+  const { name } = req.body;
+  const newUser = {
+    id: uuidv4(),
+    name,
   };
-  
+  database.push(newUser);
+  res.status(201).json({
+    message: `POST - /api`,
+    data: newUser,
+    metadata: {
+      hostname: req.hostname,
+      method: req.method,
+    },
+  });
 });
 
-router.post("/", (req, res) => {
- const { data }= req.body;
- database.push({id: uuidv4(), ...data}); // add the data to the database
- res.status(201).json({
-    message:"POST - /api", 
-    data: database[database.length - 1], // return the last item in the database
-    metadata: {hostname: req.hostname, method: req.method},
-});
-  
-});
+router.patch('/users/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const user = database.find((item) => item.id === id);
 
-router.patch("/:id", (req, res)=>{ // update select data in the database
-  const {id} = req.params;
-  const {data} = req.body;
-  const singleId = database.find((singleId)=> singleId.id === id); // find the id in the database
-  if (singleId){
-    database.push({id, ...data}); // add the data to the database
+  if (user) {
+    user.name = name;
     res.status(200).json({
-      message: "PATCH - API by ID",
-      data: singleId,
+      message: `PATCH by ID - /api`,
+      data: user,
       metadata: {
         hostname: req.hostname,
         method: req.method,
       },
     });
-  };
-  
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
 });
 
+router.delete('/users/:id', (req, res, next) => {
+  const { id } = req.params;
+  const index = database.findIndex((item) => item.id === id);
 
+  if (index !== -1) {
+    database.splice(index, 1);
+    res.status(200).json({
+      message: `DELETE by ID - /api`,
+      metadata: {
+        hostname: req.hostname,
+        method: req.method,
+      },
+    });
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
+});
 
 module.exports = router;
